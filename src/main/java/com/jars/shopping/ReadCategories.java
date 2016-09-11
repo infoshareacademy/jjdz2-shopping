cff2034248a578df8e10dc1099b72bd5412d3fca
Marcin P <marcin.pankowski@gmail.com>
2016-09-11 21:01:27 +0200

    category list

--- src/main/java/com/jars/shopping/ReadCategories.java
+++ src/main/java/com/jars/shopping/ReadCategories.java
@@ -1,11 +1,20 @@
 package com.jars.shopping;
 
+import java.util.List;
+
 /**
  * Created by marcinpankowski on 11.09.16.
  *
  * Read xml file with categories, from allegro partner
  **/
 public class ReadCategories {
+    public List<Category> categories = new ArrayList<Category();
+
+               // czytaj xml&        wcztuje elemnty do obiektu Category&                obiekt do listy
+
+
+    }
+
 
 
 
