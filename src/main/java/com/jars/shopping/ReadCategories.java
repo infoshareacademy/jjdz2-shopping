15e214d873ddb36798f8a2311f1198b3caa3529f
Marcin P <marcin.pankowski@gmail.com>
2016-09-11 20:18:27 +0200

    add ReadCategories class, it will read and convert allegro file to objects

---
+++ src/main/java/com/jars/shopping/ReadCategories.java
@@ -0,0 +1,12 @@
+package com.jars.shopping;
+
+/**
+ * Created by marcinpankowski on 11.09.16.
+ *
+ * Read xml file with categories, from allegro partner
+ **/
+public class ReadCategories {
+
+
+
+}
