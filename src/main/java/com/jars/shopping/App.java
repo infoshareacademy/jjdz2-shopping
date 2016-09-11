6af3824e184b9118c2147effa9a97e97c89584f0
Marcin P <marcin.pankowski@gmail.com>
2016-09-11 20:12:00 +0200

    initial

---
+++ src/main/java/com/jars/shopping/App.java
@@ -0,0 +1,10 @@
+package com.jars.shopping;
+
+/**
+ * Created by marcinpankowski on 11.09.16.
+ */
+public class App {
+    public static void main(String[] args) {
+        //let's start
+    }
+}
