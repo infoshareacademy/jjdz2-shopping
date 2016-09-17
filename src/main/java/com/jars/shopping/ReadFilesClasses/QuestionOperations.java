a5db749b77a22ff74a1511425a977de6b50f4e63
Nataliia <lesovaja.nat@yandex.ru>
2016-09-17 20:40:29 +0200

    .json structure

--- src/main/java/com/jars/shopping/ReadFilesClasses/QuestionOperations.java
+++ src/main/java/com/jars/shopping/ReadFilesClasses/QuestionOperations.java
@@ -1,9 +1,15 @@
 package com.jars.shopping.ReadFilesClasses;
 
+import com.jars.shopping.POJOs.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by keehoo on 12.09.2016.
  */
 public class QuestionOperations {
+    List<Question> catNames = new ArrayList<Question>();
     // TODO: In this this class we should have all the methods that deal with getting questions from JSON and returning them as strings
     //
 }
