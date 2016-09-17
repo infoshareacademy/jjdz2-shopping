3eb3ef4f4e48974477499a37b278f2e981793da4
Nataliia <lesovaja.nat@yandex.ru>
2016-09-17 20:43:56 +0200

    some infos for QuestionOperations class ver2

--- src/main/java/com/jars/shopping/ReadFilesClasses/QuestionOperations.java
+++ src/main/java/com/jars/shopping/ReadFilesClasses/QuestionOperations.java
@@ -9,8 +9,8 @@ import java.util.List;
  * Created by keehoo on 12.09.2016.
  */
 public class QuestionOperations {
-    List<Question> catNames = new ArrayList<Question>();
+    List<Question> questionList = new ArrayList<Question>();
     //http://stackoverflow.com/questions/2591098/how-to-parse-json-in-java
     // TODO: In this this class we should have all the methods that deal with getting questions from JSON and returning them as strings
-    //
+    // read json & parse json to Question objects & assign objects to list (above) & return filled List questionList
 }
