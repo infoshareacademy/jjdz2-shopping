71197d847a0d4bfc98c5771e8cb2315db9f4962a
Marcin P <marcin.pankowski@gmail.com>
2016-09-18 20:19:02 +0200

    JS-29 create link generator for ebay

--- src/main/java/com/jars/shopping/LinkGenerator/GenerateLink.java
+++ src/main/java/com/jars/shopping/LinkGenerator/GenerateLink.java
@@ -2,18 +2,26 @@ package com.jars.shopping.LinkGenerator;
 
 
 public class GenerateLink {
-
+    private List<String> linkList = new ArrayList<String>();
 
 
     public List<String> AllegroLinksGenerator(List<int> catIds){
-        List<String> linkList = new ArrayList<String>();
-
         if(!catIds.isEmpty()){
             for(String idNumber: catNames){
                 linkList.add("http://allegro.pl/some-cat-" + Integer.toString(idNumber));
             }
         }
-    return linkList;
+        return linkList;
+    }
+
+    public List<String> EbayLinksGenerator(List<String> catNames){
+        if(!catNames.isEmpty()){
+            for(String catName: catNames){
+                catName = catName.replaceAll("\\s+","-")
+                linkList.add("http://www.ebay.com/sch/" + catName);
+            }
+        }
+        return linkList;
     }
 
 }
