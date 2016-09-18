ba4a5061abddc6a20489e55653cca4b0004b39f1
Nataliia <lesovaja.nat@yandex.ru>
2016-09-18 20:58:40 +0200

    JS-19 - new class Questionary

---
+++ src/main/java/com/jars/shopping/POJOs/models/Questionary.java
@@ -0,0 +1,16 @@
+package com.jars.shopping.POJOs.models;
+
+import java.util.List;
+
+/**
+ * Created by nlisova on 18.09.16.
+ */
+public class Questionary {
+    List<Question> qestions;
+
+    public List<Question> getQuestions() {
+
+        return qestions;
+    }
+
+}
