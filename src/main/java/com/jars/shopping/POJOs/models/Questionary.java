66d4b823e360dbbed257924ca479f80fb020d2fd
Krzysztof Jedrzejewski <krzysztof.jedrzejewski.gd@gmail.com>
2016-09-20 21:20:00 +0200

    JS-26 an attempt to import the Jackson ObjectMapper library to parse JSON files

--- src/main/java/com/jars/shopping/POJOs/models/Questionary.java
+++ src/main/java/com/jars/shopping/POJOs/models/Questionary.java
@@ -1,6 +1,11 @@
 package com.jars.shopping.POJOs.models;
 
+import java.io.File;
+import java.net.URL;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 /**
  * Created by nlisova on 18.09.16.
@@ -12,5 +17,17 @@ public class Questionary {
 
         return qestions;
     }
+
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonInString = "{'name' : 'mkyong'}";
+
+    //JSON from file to Object
+    Staff obj = mapper.readValue(new File("c:\\file.json"), Staff.class);
+
+    //JSON from URL to Object
+    Staff obj = mapper.readValue(new URL("http://mkyong.com/api/staff.json"), Staff.class);
+
+    //JSON from String to Object
+    Staff obj = mapper.readValue(jsonInString, Staff.class);
 
 }
