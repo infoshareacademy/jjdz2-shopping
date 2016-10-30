e3b3e781810254a04f7f070914873453d460da50
Krzysiek Kubicki <kkubicki2@gmail.com>
2016-10-30 20:12:49 +0100

    Dodano do pomu biblioteke jstl

--- src/main/java/com/jars/shopping/LogowanieDoSystemu/Login.java
+++ src/main/java/com/jars/shopping/LogowanieDoSystemu/Login.java
@@ -1,11 +1,13 @@
 package com.jars.shopping.LogowanieDoSystemu;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by keehoo on 30.10.16.
@@ -18,6 +20,10 @@ public class Login extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String welcome = "Welcome to the login page";
 
+        req.setAttribute("welcome", welcome);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/dict.jsp");
+        dispatcher.forward(req, resp);
     }
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,11 +1,5 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: keehoo
-  Date: 30.10.16
-  Time: 19:43
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Logowanie do systemu</title>
@@ -14,6 +8,17 @@
 
 <div>
     <h1> Log in </h1>
+</div>
+
+<div>
+    <div>Jakas tam lista:</div>
+    <div>
+        <ul>
+            <c:forEach items="${lista}" var="item">
+                <li> ${item}: </li>
+            </c:forEach>
+        </ul>
+    </div>
 </div>
 </body>
 </html>
