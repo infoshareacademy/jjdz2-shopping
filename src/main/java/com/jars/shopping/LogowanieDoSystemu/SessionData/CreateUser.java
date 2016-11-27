6514617d67dded02a65aae6e52c0ce18702c5594
Krzysiek Kubicki <kkubicki2@gmail.com>
2016-11-27 21:00:39 +0100

    Facebook login added, data base added, user creation, CRUD users - started

---
+++ src/main/java/com/jars/shopping/LogowanieDoSystemu/SessionData/CreateUser.java
@@ -0,0 +1,40 @@
+package com.jars.shopping.LogowanieDoSystemu.SessionData;
+
+import com.jars.shopping.Users.User;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by keehoo on 27.11.16.
+ */
+
+@WebServlet (urlPatterns = "/createUser")
+public class CreateUser extends HttpServlet {
+
+    @Inject
+    UserDao userDao;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        System.out.println("Stworzono uzytkownika "+ username);
+
+        userDao.saveUserInDataBase(new User(username, password));
+
+
+    }
+}
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -19,20 +19,17 @@
 </div>
 
 <div>
-    <form method="post" action="translate">
-        login: <input type="text" name="login">
-    </form>
-    <form method="post" action="translate">
+
+    <form method="post" action="createUser">
+        login   : <input type="text" name="username">
         password: <input type="password" name="password">
         <input type="submit" value="login">
+        <input type="submit" value="createUser">
     </form>
 
     <a href = "/twitter"> TWITTER</a>
     <a href = "/facebook">FACEBOOK</a>
-
 </div>
-
-
 
 
 </body>
