3265f2152d44e9e3e1ca365ab342c2158f1e3b92
Krzysiek Kubicki <kkubicki2@gmail.com>
2016-11-27 19:25:30 +0100

    Filters and Session files initial commit

--- src/main/java/com/jars/shopping/LogowanieDoSystemu/SessionData/SessionData.java
+++ src/main/java/com/jars/shopping/LogowanieDoSystemu/SessionData/SessionData.java
@@ -1,7 +1,31 @@
 package com.jars.shopping.LogowanieDoSystemu.SessionData;
 
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
 /**
  * Created by keehoo on 26.11.16.
  */
-public class SessionData {
+
+@SessionScoped
+public class SessionData implements Serializable{
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isLogged() {
+        return isLogged;
+    }
+
+    public void setLogged(boolean logged) {
+        isLogged = logged;
+    }
+
+    private boolean isLogged = false;
+    private String name;
 }
