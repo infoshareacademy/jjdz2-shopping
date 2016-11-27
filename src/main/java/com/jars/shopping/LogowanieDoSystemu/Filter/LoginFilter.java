d3929d33d9e2962e5b76ca27fb89cf9f1b8445f5
Krzysiek Kubicki <kkubicki2@gmail.com>
2016-11-27 19:25:30 +0100

    Filters and Session files initial commit

--- src/main/java/com/jars/shopping/LogowanieDoSystemu/Filter/LoginFilter.java
+++ src/main/java/com/jars/shopping/LogowanieDoSystemu/Filter/LoginFilter.java
@@ -1,12 +1,16 @@
 package com.jars.shopping.LogowanieDoSystemu.Filter;
 
-
+import com.jars.shopping.LogowanieDoSystemu.SessionData.SessionData;
+import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = "/*")
 public class LoginFilter implements Filter {
+
+    @Inject
+    SessionData sessionData;
 
 
     @Override
@@ -19,6 +23,12 @@ public class LoginFilter implements Filter {
 
         System.out.println("Filter !!!!");
         filterChain.doFilter(servletRequest, servletResponse);
+
+        if (!sessionData.isLogged()) {
+            //przekierowanie na logowanie
+          //  HttpServletRequest)servletResponse sendRedirect
+
+        }
     }
 
     @Override
