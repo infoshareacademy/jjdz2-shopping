6a66f913a33b8c1e71455252ad9777882cd58f26
Krzysiek Kubicki <kkubicki2@gmail.com>
2016-12-02 21:30:18 +0100

    Logowanie do facebooka dziala poprzez dodanie re-routingu portu 80 na 8080

--- src/main/java/com/jars/shopping/LogowanieDoSystemu/FacebookCallback.java
+++ src/main/java/com/jars/shopping/LogowanieDoSystemu/FacebookCallback.java
@@ -12,31 +12,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.List;
 
-
-
-/**
- * Created by keehoo on 27.11.16.
- */
 
 @WebServlet(urlPatterns = "/facebookcallback")
 public class FacebookCallback extends HttpServlet{
 
-    private ServiceBuilder createService() {
-        return new ServiceBuilder()
-                .provider(TwitterApi.class)
-                .apiKey("693169580831907")
-                .apiSecret("33429cb2d1c1fd80842cd1679925d8be");
-    }
+    private String code;
+
+    /*
+    * This requires rerouting port 80 to 8080 details here:
+    * # iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
+    * */
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        OAuthService service = createService().build();
-        String code = req.getParameter("code");
-        System.out.println("authorization code: "+code);
+        OAuthService service = new ServiceBuilder()
+                .provider(FacebookApi.class)
+                .apiKey("693169580831907")
+                .apiSecret("33429cb2d1c1fd80842cd1679925d8be")
+                .callback("http://localhost/facebookcallback")
+                .build();
 
+        Enumeration<String> parameterNames = req.getParameterNames();
+
+
+        if (parameterNames.hasMoreElements()) {
+            System.out.println(parameterNames.nextElement());
+
+            code = req.getParameter("code");
+            System.out.println("authorization code: " + code);
+        }
+
+        else {code = "brak kodu";}
         Verifier verifier = new Verifier(code);
+
         Token accessToken = service.getAccessToken(FacebookLogin.EMPTY_TOKEN, verifier);
         System.out.println("(if your curious it looks like this: " + accessToken + " )");
 
@@ -48,7 +61,5 @@ public class FacebookCallback extends HttpServlet{
         System.out.println();
         System.out.println(response.getCode());
         System.out.println(response.getBody());
-
-
     }
 }
