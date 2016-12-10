887c489b6aff812a1387c1bd21b9dde973e101d2
Krzysiek Kubicki <kkubicki2@gmail.com>
2016-12-10 20:22:45 +0100

    In app user creation/ logging in and dummy data

--- src/main/java/com/jars/shopping/LogowanieDoSystemu/Filter/LoginFilter.java
+++ src/main/java/com/jars/shopping/LogowanieDoSystemu/Filter/LoginFilter.java
@@ -1,6 +1,9 @@
 package com.jars.shopping.LogowanieDoSystemu.Filter;
 
 import com.jars.shopping.LogowanieDoSystemu.SessionData.SessionData;
+import com.jars.shopping.LogowanieDoSystemu.SessionData.UserDao;
+import com.jars.shopping.Users.User;
+
 import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -11,10 +14,18 @@ public class LoginFilter implements Filter {
 
     @Inject
     SessionData sessionData;
+    @Inject
+    UserDao userDao;
+
 
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+
+        userDao.saveUserInDataBase(new User("krzysiek", "password", "InAppCreate", true));
+        userDao.saveUserInDataBase(new User("niekrzysiek", "password", "InAppCreate", false));
+        userDao.saveUserInDataBase(new User("nie222krzysiek", "password", "InAppCreate", false));
+        userDao.saveUserInDataBase(new User("nie222kr333zysiek", "password", "InAppCreate", false));
 
     }
 
