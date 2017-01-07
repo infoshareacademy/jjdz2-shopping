2e3c3990d8faf616fb4d7c85938e2587268cce39
Marcin P <marcin.pankowski@gmail.com>
2017-01-07 19:51:17 +0100

    add fire event with list of products

--- src/main/java/com/jars/shopping/ProductList/CategoryToListOfProducts.java
+++ src/main/java/com/jars/shopping/ProductList/CategoryToListOfProducts.java
@@ -1,9 +1,13 @@
 package com.jars.shopping.ProductList;
 
+import com.jars.shopping.WebForm.ChosenCategories;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Created by marcinpankowski on 18.12.16.
@@ -19,7 +23,7 @@ public class CategoryToListOfProducts {
     @EJB
     ProductsAllegroService serviceAllegro;
 
- /*   public void getNewProducts(@Observes ChosenCategories event) {
+  public void getNewProducts(@Observes ChosenCategories event) {
         // symulacja danych od Natali - 2xLista<String> -> event.categoriesEbay i event.categoriesAllegro
        /// List<String> categoriesEN = new ArrayList<String>();
        /// categoriesEN.add("CAT"); categoriesEN.add("DOG");
@@ -27,15 +31,15 @@ public class CategoryToListOfProducts {
 
        List<Products> newAllProductsList = null;
 
-        for(String nameCatE : event.categoriesEbay) {
+        for(String nameCatE : event.getEbay()){
             newAllProductsList.addAll(serviceEbay.getProductEbayListFromUrl(nameCatE.toString()));
         }
 
-        for(String nameCatA : event.categoriesAllegro) {
+        for(String nameCatA : event.getAllegro()) {
             newAllProductsList.addAll(serviceAllegro.getProductAllegroListFromUrl(nameCatA.toString()));
         }
 
         eventList.fire(new ProductListEvent(newAllProductsList));
 
-    }*/
+    }
 }
