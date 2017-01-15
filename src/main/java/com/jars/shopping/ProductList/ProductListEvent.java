9699b07f18dd0fa7fb44cc425446597a660cfa5e
Marcin P <marcin.pankowski@gmail.com>
2017-01-15 18:43:00 +0100

    added two list of generated products for event for Natalia

--- src/main/java/com/jars/shopping/ProductList/ProductListEvent.java
+++ src/main/java/com/jars/shopping/ProductList/ProductListEvent.java
@@ -6,18 +6,30 @@ import java.util.List;
  * Created by marcinpankowski on 18.12.16.
  */
 public class ProductListEvent {
-    private List<Products> allProductsList;
+    private List<Products> allProductsListAllegro;
+    private List<Products> allProductsListEbay;
 
 
-    public ProductListEvent(List<Products> allProductsList) {
-        this.allProductsList = allProductsList;
+    public ProductListEvent(List<Products> allProductsListEbay,List<Products>  allProductsListAllegro) {
+        this.allProductsListEbay = allProductsListEbay;
+        this.allProductsListAllegro = allProductsListAllegro;
     }
 
-    public List<Products> getAllProductsList() {
-        return allProductsList;
+
+
+    public List<Products> getAllProductsListEbay() {
+        return allProductsListEbay;
     }
 
-    public void setAllProductsList(List<Products> allProductsList) {
-        this.allProductsList = allProductsList;
+    public void setAllProductsListEbay(List<Products> allProductsListEbay) {
+        this.allProductsListEbay = allProductsListEbay;
+    }
+
+    public List<Products> getAllProductsListAllegro() {
+        return allProductsListAllegro;
+    }
+
+    public void setAllProductsListAllegro(List<Products> allProductsListAllegro) {
+        this.allProductsListAllegro = allProductsListAllegro;
     }
 }
